65321a09d63c036637a79ed3983fc4cd325b5fb2
ejlee925 <ejlee.github@gmail.com>
2024-10-03 18:37:00 +0900

    5

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.see_all_tweets), # root 경로\
+    path("api/v1/tweets",views.get_all_tweets_list),
+    path("api/v1/users/<int:user_id>/tweets",views.get_tweets_list),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tweet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from users.models import User
+from .serializers import TweetSerializer
 
 
 def see_all_tweets(request):
@@ -10,3 +14,23 @@ def see_all_tweets(request):
         "all_tweets.html",
         {"tweets" : tweets, "title": "All Tweets"}
     )
+
+
+@api_view(["GET"])
+def get_all_tweets_list(request):
+    tweets = Tweet.objects.all()
+    serializer = TweetSerializer(tweets,many=True)
+    return Response({"result": "exist", "data": serializer.data})
+
+@api_view(["GET"])
+def get_tweets_list(request,user_id):
+    try: 
+        user = User.objects.filter(id=user_id).get(pk = user_id)
+        try:
+            tweet = Tweet.objects.filter(user = user)
+            serializer = TweetSerializer(tweet,many=True)
+            return Response({"result": "exist", "data": serializer.data})
+        except:
+            return Response({"result": "tweets not found","data": ""})
+    except User.DoesNotExist:
+        raise Response({"result": "user not found","data": ""})
